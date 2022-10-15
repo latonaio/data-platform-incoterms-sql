6cf1411bd6bec3a5b3c1beafcbb5f86263cd5e5f
Yurino Oga <yurino.o@latona.io>
2022-10-15 18:43:16 +0900

    release

--- data-platform-incoterms-sql-incoterms-data.sql
+++ data-platform-incoterms-sql-incoterms-data.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `data_platform_incoterms_incoterms_data`
 (
-    `Incoterms`               varchar(3) NOT NULL,
+    `Incoterms`               varchar(3) NOT NULL,   -- 名称変更
     PRIMARY KEY (`Incoterms`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
--- data-platform-incoterms-sql-incoterms-text-data.sql
+++ data-platform-incoterms-sql-incoterms-text-data.sql
@@ -1,8 +1,8 @@
 CREATE TABLE `data_platform_incoterms_incoterms_text_data`
 (
-    `Incoterms`          varchar(3) NOT NULL,
+    `Incoterms`          varchar(3) NOT NULL,　　　　　　 -- 名称変更
     `Language`           varchar(3) NOT NULL,
-    `IncotermsName`      varchar(100) DEFAULT NULL,
+    `IncotermsName`      varchar(100) DEFAULT NULL,　　　-- 名称変更
     PRIMARY KEY (`Incoterms`, `Language`),
     CONSTRAINT `DataPlatformIncotermsIncotermsTextData_fk` FOREIGN KEY (`Incoterms`) REFERENCES `data_platform_incoterms_incoterms_data` (`Incoterms`)
 ) ENGINE = InnoDB
