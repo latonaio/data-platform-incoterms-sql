8d216b0e51db41ce2dc7362b820bb28a7f92f4cb
MiyuYokoyama0109 <miyu.y@latona.io>
2023-02-22 13:14:58 +0900

    release

--- data-platform-incoterms-sql-incoterms-data-setup.sql
+++ data-platform-incoterms-sql-incoterms-data-setup.sql
@@ -2,20 +2,20 @@ LOCK TABLES `data_platform_incoterms_incoterms_data` WRITE;
 
 INSERT INTO `data_platform_incoterms_incoterms_data` (`Incoterms`)
 VALUES
-    ('UN');
-    ('FOB');
-    ('FH');
-    ('FCA');
-    ('FAS');
-    ('EXW');
-    ('DES');
-    ('DEQ');
-    ('DDU');
-    ('DDP');
-    ('DAF');
-    ('CPT');
-    ('CIP');
-    ('CIF');
+    ('UN'),
+    ('FOB'),
+    ('FH'),
+    ('FCA'),
+    ('FAS'),
+    ('EXW'),
+    ('DES'),
+    ('DEQ'),
+    ('DDU'),
+    ('DDP'),
+    ('DAF'),
+    ('CPT'),
+    ('CIP'),
+    ('CIF'),
     ('CFR');
 UNLOCK TABLES;
 
